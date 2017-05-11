9ad609091c294c886a0461fa95e784ff2ade8f19
Karin Chung <karin.chung13@gmail.com>
2017-05-11 13:24:47 -0700

    group painting

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const http = require('http')
+const socketIO = require('socket.io')
+const PORT = 3000
+
+var app = express()
+var httpServer = http.Server(app)
+var wsServer = socketIO(httpServer)
+
+app.get('/', function(req, res) {
+  res.sendFile('index.html', {root:__dirname})
+})
+
+wsServer.on('connection', function(socket) {
+  console.log('a user connected')
+  socket.on('dragged', function(d) {
+    // console.log('someone is painting', d)
+    wsServer.emit('global-drag', d)
+  })
+})
+
+httpServer.listen(PORT, function() {
+  console.log('🤘 http server started on port: ', PORT)
+})
